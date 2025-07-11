eb27d8e0b1501ee39b2121c1779375a799c1257a
ruziba3vich <rruzibayevich@gmail.com>
2025-07-11 17:33:20 +0500

    feat js_service: add config loader

---
+++ pkg/config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+type (
+	Config struct {
+		AppPort, JsContainerName, ContainerWorkingDir, JsSourceFileName, JsExecutableName, LogPath string
+		CompilationTimeout, ExecutionTimeout                                                       time.Duration
+	}
+)
+
+func NewConfig() *Config {
+	_ = godotenv.Load()
+	return &Config{
+		LogPath:             getEnv("LOG_PATH", "app.log"),
+		AppPort:             getEnv("APP_PORT", "703"),
+		JsContainerName:     getEnv("JS_CONTAINER_NAME", "cpp-executor"),
+		JsExecutableName:    getEnv("JS_EXECUTABLE_NAME", "program"),
+		JsSourceFileName:    getEnv("JS_SOURCE_FILE_NAME", "main.cpp"),
+		ContainerWorkingDir: getEnv("CONTAINER_WORKING_DIR", "/app"),
+		CompilationTimeout:  getEnvTime("COMPILATION_TIME_OUT", 15),
+		ExecutionTimeout:    getEnvTime("EXECUTION_TIME_OUT", 60),
+	}
+}
+
+func getEnv(key, fallback string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return fallback
+}
+
+func getEnvTime(key string, fallback int) time.Duration {
+	if value := os.Getenv(key); value != "" {
+		v, err := strconv.Atoi(value)
+		if err == nil {
+			return time.Duration(v) * time.Second
+		}
+
+	}
+	return time.Duration(fallback) * time.Second
+}
